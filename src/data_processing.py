7609d3c961dcd271a35460338927e301e28c6105
MobeenZar <mobeen_zar@hotmail.com>
2025-12-17 13:21:09 +0300

    Initial creation

--- src/data_processing.py
+++ src/data_processing.py
@@ -1,12 +1,13 @@
-def add_numbers(a, b):  
- """Function to add two numbers."""  
- return a + b  
- 
+def add_numbers(a, b):
+    """Function to add two numbers."""
+    return a + b
 
-def multiply_numbers(a, b):  
- """Function to multiply two numbers."""  
- return a * b  
-  
-if __name__ == "__main__":  
- print(add_numbers(2, 3))  
- print(multiply_numbers(2, 3)) 
+
+def multiply_numbers(a, b):
+    """Function to multiply two numbers."""
+    return a * b
+
+
+if __name__ == "__main__":
+    print(add_numbers(2, 3))
+    print(multiply_numbers(2, 3))
